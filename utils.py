c3f4bbe8d8ec1302566c2b54e77803613b53b973
Will Behm <willbehm@Wills-MacBook-Pro.local>
2025-11-12 12:58:15 -0500

    Added parsing/cleaning for sentences

--- utils.py
+++ utils.py
@@ -1,13 +1,11 @@
 import nltk
 import re
 import string
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 
 """  
-Copy and paste yo story into a .txt and put it into this bad boy.
-I'm absolutely 100% sure it will definitely work and will
-not need any more cleaning at all whatsoever.
+Function to preprocess a given corpus. Can be set to return sentences or just a straight flow of tokens, all will have stop words and unnecessary punctuation removed
 
 IN: .txt
 OUT: a list of words in order of appearance, stop words and punctuation removed
@@ -17,25 +15,45 @@ You should be able to call:
 from utils import preprocess_corpus
 if you are working on a .py file in the same directory
 """
-
-def preprocess_corpus(txt_file):
-    #read in the file
-    with open(txt_file, "r", encoding="utf-8") as f:
-        text = f.read()
-
-    #get rid of all whitespace
-    text = re.sub(r'\s+', ' ', text)
-    #Lowercases, strip whitespace
-    text = text.lower().strip()
-    text.replace("\n", " ")
-
-    tokens = word_tokenize(text)
-
+def clean_tokens_helper(tokens):
     stop_words = set(stopwords.words('english'))
     punctuation = set(string.punctuation)
+    
     clean_tokens = [
         word.lower()
         for word in tokens
         if word.isalpha() and word.lower() not in stop_words
         ]
     return clean_tokens
+
+def preprocess_corpus(txt_file, sentences=False):
+    #read in the file
+    with open(txt_file, "r", encoding="utf-8") as f:
+        text = f.read()
+
+    #get rid of all whitespace
+        text = re.sub(r'\s+', ' ', text)
+
+    if sentences:
+        #tokenize the sentences (returns a list of sentences)
+        sent_tokens = sent_tokenize(text)
+        clean_sentences = []
+        #Now process each sentence
+        for sentence in sent_tokens:
+            #tokenize it
+            tokens = word_tokenize(sentence)
+            #remove stop words and whatnot
+            clean_sentences.append(clean_tokens_helper(tokens))
+        return clean_sentences
+
+    else:
+        
+        #Lowercases, strip whitespace
+        text = text.lower().strip()
+        text.replace("\n", " ")
+
+        tokens = word_tokenize(text)
+        clean_tokens =clean_tokens_helper(tokens)
+        return clean_tokens
+
+        
